c710527ab4b6e3d3154d9b3fae90496970d73a6f
Leo Komissarov <komissarov@scm.com>
2022-02-03 20:42:43 +0000

    Fix typo

--- pltmov/__init__.py
+++ pltmov/__init__.py
@@ -1,7 +1,7 @@
 from .movie import Movie
 from shutil import which
 
-if which('ffmppeg') is None:
+if which('ffmpeg') is None:
     raise ImportError(f"This package requires the 'ffmpeg' binary to be installed.")
     
 __all__ = ['Movie']
